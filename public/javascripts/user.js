3a72cfb44202bff30dccaf46d8a8b71792781784
marko-ivan <personaldeveloperbg@gmail.com>
2019-06-16 11:32:58 -0700

    update table using datatable

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -1,7 +1,25 @@
 $(document).ready(function(){
-  $('table').dataTable( {
-  paginate: true,
-  scrollX: true,
-  scrollY: 600
-} );
+  $('table').dataTable({
+    'paginate': true,
+    'scrollX': true,
+    'scrollY': 600,
+    'columnDefs': [
+      {
+        'targets': 0,
+        'render': function(data, type, row, meta){
+          // console.log(data, type, row, meta);
+          if(type === 'display'){
+            data = '<div class="checkbox"><input type="checkbox" class="dt-checkboxes"><label></label></div>';
+          }
+          return data;
+        },
+        'checkboxes': {
+          'selectRow': true,
+          'selectAllRender': '<div class="checkbox"><input type="checkbox" class="dt-checkboxes"><label></label></div>'
+        }
+      },
+    ],
+    'select': 'multi',
+    'order': [[1, 'asc']]
+  });
 })
