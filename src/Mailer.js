d873f8c83231677e4c4a4cc2b90958144399553e
Ahmed Sabry <abou7mied.soft@gmail.com>
2020-03-03 22:36:43 +0200

    Encode HTML entities in the mail

--- src/Mailer.js
+++ src/Mailer.js
@@ -1,14 +1,18 @@
 const nodemailer = require('nodemailer');
+const Entities = require('html-entities').AllHtmlEntities;
 const config = require('../config/config');
+
+const entities = new Entities();
 
 class Mailer {
   async sendMail(data, emailAddress) {
+    const encodedData = entities.encode(data);
     const transporter = nodemailer.createTransport(config.smtp);
     const info = await transporter.sendMail({
       from: config.emailFrom, // sender address
       to: emailAddress, // list of receivers
       subject: 'Logs for recent actions performed', // Subject line
-      html: `<pre>${data}</pre>`, // html body
+      html: `<pre>${encodedData}</pre>`, // html body
     });
     console.log('Message sent: %s', info.messageId);
   }
