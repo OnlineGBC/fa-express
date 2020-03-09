180ecb111f810d608b71c0d38199f296917b911e
hollywood-itdev <hollywood.itdev@protonmail.com>
2020-03-09 15:33:51 +0900

    new branch

--- routes/api/action.js
+++ routes/api/action.js
@@ -1,13 +1,11 @@
-const express = require('express');
-const actionUtils = require('../../src/actionUtils');
+const express = require("express");
+const actionUtils = require("../../src/actionUtils");
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const { body, files } = req;
-  const {
-    fileName, extension, menuTitle, fileContents,
-  } = body;
+  const { fileName, extension, menuTitle, fileContents } = body;
   if (fileName && extension && menuTitle && (fileContents || files.file)) {
     try {
       const fileInfo = {};
@@ -18,25 +16,23 @@ router.post('/', async (req, res) => {
       }
       await actionUtils.saveAction(fileName, extension, menuTitle, fileInfo);
       res.json({
-        status: 'success',
+        status: "success"
       });
     } catch (error) {
-      res.status(500)
-        .json({
-          status: 'error',
-          error: error.message,
-        });
+      res.status(500).json({
+        status: "error",
+        error: error.message
+      });
     }
   } else {
-    res.status(400)
-      .json({
-        status: 'error',
-        error: 'invalid data',
-      });
+    res.status(400).json({
+      status: "error",
+      error: "invalid data"
+    });
   }
 });
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   const actions = await actionUtils.listActions();
   res.json(actions);
 });
