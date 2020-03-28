5f14666e209d9b3ebb8b454942c5991871e2df8e
Muddassir Ahmed <muddassir.ah@gmail.com>
2020-03-28 17:39:13 +0500

    http fix

--- app.js
+++ app.js
@@ -13,18 +13,6 @@ require('./container');
 
 const app = express();
 
-app.enable('trust proxy');
-
-app.use (function (req, res, next) {
-        if (req.secure) {
-                // request was via https, so do no special handling
-                next();
-        } else {
-                // request was via http, so redirect to https
-                res.redirect('https://' + req.headers.host + req.url);
-        }
-});
-
 app.use(session({ secret: 'secret' }));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
